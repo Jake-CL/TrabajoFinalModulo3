3f23b4340b04177b24623ce23d1850e6fe9b917c
Jake-CL <fariash@hotmail.com>
2025-08-27 18:59:03 -0400

    Se creao archivo musicApi.jsx

--- src/api/musicApi.js
+++ src/api/musicApi.js
@@ -2,11 +2,10 @@ import axios from 'axios';
 
 const CORS_PROXY_URL = 'https://cors-anywhere.herokuapp.com/';
 const DEEZER_BASE_URL = 'https://api.deezer.com';
-//const API_KEY = '2';
+
 const searchTracks = async (query) => {
 try {
 const response = await axios.get(`${CORS_PROXY_URL}${DEEZER_BASE_URL}/search?q=${query}`);
-//const response = await axios.get(`https://www.theaudiodb.com/api/v1/json/${API_KEY}/searchtrack.php?s=${query}`);
 
 return response.data.data;
 } catch (error) {
